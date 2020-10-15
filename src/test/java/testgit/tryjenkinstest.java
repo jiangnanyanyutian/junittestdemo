c7c15f41fff172a298617156c21165fd0cf14b7e
jiangnanyanyutian <zhangliujie200712@126.com>
2020-10-15 21:05:04 +0800

    jenkinstest

---
+++ src/test/java/testgit/tryjenkinstest.java
@@ -0,0 +1,14 @@
+package testgit;
+
+import org.testng.annotations.Test;
+
+public class tryjenkinstest {
+    @Test
+        public void addfunction(){
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}
