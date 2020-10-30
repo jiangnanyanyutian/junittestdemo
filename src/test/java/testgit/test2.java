8e5a6a0658c0613cd67643a0e126079eaded4217
jiangnanyanyutian <zhangliujie200712@126.com>
2020-10-30 16:03:45 +0800

    引入allure配置文件

---
+++ src/test/java/testgit/test2.java
@@ -0,0 +1,24 @@
+package testgit;
+
+import org.testng.annotations.Test;
+
+public class test2{
+    @Test
+    public void addfunction4(){
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+
+
+    @Test
+    public void addfunction5(){
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}
