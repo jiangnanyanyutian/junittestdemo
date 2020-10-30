ecb8836d6694f4894bc851eed54a0b13cde39ad3
jiangnanyanyutian <zhangliujie200712@126.com>
2020-10-30 15:56:19 +0800

    引入allure配置文件

--- src/test/java/testgit/tryjenkinstest.java
+++ src/test/java/testgit/tryjenkinstest.java
@@ -1,14 +1,25 @@
 package testgit;
 
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 public class tryjenkinstest {
     @Test
-        public void addfunction(){
+        public void addfunction1(){
         int a=2;
         int b=3;
         int c=a+b;
         System.out.println(c);
+
+    }
+
+
+    @Test
+    public void addfunction2(){
+        int a=2;
+        int b=3;
+        int c=a+b;
+                System.out.println(c);
 
     }
 }
